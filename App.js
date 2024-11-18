df2d4b896d6273f5453b20afdb6c6cb7c1652b54
DivyaReddy-create <k.divyabharathi06@gmail.com>
2024-11-18 09:19:22 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,32 +1,37 @@
-
-import React,{useState} from 'react'
-import Products from './Products';
-
+import React ,{useState} from 'react';
+import TodoList from './TodoList';
 const App = () => {
-  const [search,setSearch] = useState('');
-  const [data,setData] = useState([]);
-  const YOUR_APP_ID = "2ffa4089";
-  const YOUR_APP_KEY ="a8d8dc52df92df158cdfc2afc902b0d3	";
-  const submitHandler = e =>{
-    e.preventDefault();
-    fetch(`https://api.edamam.com/search?q=${search}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=30&calories=591-722&health=alcohol-free`).then(
-      response => response.json()
-    ).then(
-      data => setData(data.hits)
-    )
-  }
-  return (
-    <div>
-      <center>
-        <h4>Food Recipe App</h4>
-        <form onSubmit={submitHandler}>
-          <input type="text" value={search} onChange={(e) => setSearch(e.target.value)}/> <br/>
-          <input type="submit" className="btn btn-primary" value="Search"/>
-        </form>
-        {data.length>=1 ? <Products  data={data}/>:null}
-      </center>
-    </div>
-  )
-}
+const [task,setTask] = useState("");
+const[todos,setTodos] = useState([]);
 
+const handler = (e) =>
+{
+    setTask(e.target.value);
+}
+const submitHandler = (e) => {
+    e.preventDefault();
+    const newTodos = [...todos,task];
+    setTodos(newTodos);
+    setTask("");
+}
+const deleteHandler = (indexValue) => {
+    const newTodos = todos.filter((todo,index) => index!==indexValue);
+    setTodos(newTodos);
+
+}
+return (
+    <div>
+        
+        <center>
+            <div  className = "card"></div>
+            <div className ="card-body"></div>
+        <h5 className="card-title">Todo Management Application</h5><br/>
+        <form onSubmit = {submitHandler}>
+            <input type = "text" name ="task" value = {task} onChange = {handler}  ></input>           
+            <input  type = "submit" value = "Add" name ="Add"></input><br></br>
+             </form> <TodoList todolist = {todos} deleteHandler = {deleteHandler}/>
+        </center>
+    </div>
+);
+}
 export default App;
