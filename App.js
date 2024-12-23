f741f379bc2f8f3310758def8a117c524717686e
DivyaReddy-create <k.divyabharathi06@gmail.com>
2024-12-23 16:12:12 +0530

    Add files via upload
    Counter with state

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import Greeting from './Greeting';
+import React,{useState} from 'react';
+import './App.css';
 const App = () => {
+  const[data,setData] = useState(0);
 
   return(
+    
     <div>
       <center>
-        <Greeting name = "divya"/>
+        <h1>{data}</h1>
+      
+        <button onClick = {() => setData(data+1)}>Increment</button>
+        <button onClick = {() => setData(data-1)}>Decrement</button>
+        <button onClick = {() => setData(0)}>Reset</button>
       </center>
     </div>
   )
