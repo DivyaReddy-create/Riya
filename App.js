e329f3f4de2a3560870ec31f3360cc8d05c7c69a
DivyaReddy-create <k.divyabharathi06@gmail.com>
2024-12-23 15:38:34 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,37 +1,13 @@
-import React ,{useState} from 'react';
-import TodoList from './TodoList';
+import React from 'react';
+import Greeting from './Greeting';
 const App = () => {
-const [task,setTask] = useState("");
-const[todos,setTodos] = useState([]);
 
-const handler = (e) =>
-{
-    setTask(e.target.value);
-}
-const submitHandler = (e) => {
-    e.preventDefault();
-    const newTodos = [...todos,task];
-    setTodos(newTodos);
-    setTask("");
-}
-const deleteHandler = (indexValue) => {
-    const newTodos = todos.filter((todo,index) => index!==indexValue);
-    setTodos(newTodos);
-
-}
-return (
+  return(
     <div>
-        
-        <center>
-            <div  className = "card"></div>
-            <div className ="card-body"></div>
-        <h5 className="card-title">Todo Management Application</h5><br/>
-        <form onSubmit = {submitHandler}>
-            <input type = "text" name ="task" value = {task} onChange = {handler}  ></input>           
-            <input  type = "submit" value = "Add" name ="Add"></input><br></br>
-             </form> <TodoList todolist = {todos} deleteHandler = {deleteHandler}/>
-        </center>
+      <center>
+        <Greeting name = "divya"/>
+      </center>
     </div>
-);
+  )
 }
 export default App;
