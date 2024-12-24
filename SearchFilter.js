03550ad6dc6221f6830eb12ceb0e65b28ccdb2b7
DivyaReddy-create <k.divyabharathi06@gmail.com>
2024-12-24 12:28:47 +0530

    Add files via upload

---
+++ SearchFilter.js
@@ -0,0 +1,20 @@
+import React,{useState} from "react";
+const App = () => {
+const[data,setData] =useState('');
+const items=["Apple","Banana","Orange","Grapes","Almonds"];
+const filterdItems = items.filter(item => item.toLowerCase().includes(data.toLowerCase()));
+console.log(data);
+return(
+    <div>
+        <center>
+        <h1>Search Filter</h1>
+            <input type = "text" placeholder="search items..." value = {data} onChange ={(e)=>setData(e.target.value)}/>
+<ul>
+{filterdItems.length > 0 ? (filterdItems.map((item,index) => <li key = {index}>{item}</li>))
+:(<li>No items found</li>)}    
+    </ul>        
+        </center>
+    </div>
+)
+}
+export default App;
