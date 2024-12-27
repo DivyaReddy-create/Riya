5f793a152ad4306295d4cdd2a47ade5b5d24a2b2
DivyaReddy-create <k.divyabharathi06@gmail.com>
2024-12-27 06:44:58 +0530

    Add files via upload

--- Form.js
+++ Form.js
@@ -1,37 +1,67 @@
-import React,{useState} from 'react';
-const App = ()=> {
-    const [data,setData]=useState({
-        username:"",
-        email:""
-    })
-    const{username,email} = data;
-    const changeHandler = (e)=>{
-        setData({...data,[e.target.name]:e.target.value})
-    }
-    const onSubmit=(e)=>{
-        e.preventDefault();
-        if(username.length <= 5){
-            alert("user name must be atleast 6 characters");
-        }
-        else{
-            console.log(data);
-            alert("Form Submitted Sucessfully")
-        }
-    }
-    return(
-<div>
-<center>
-    <form onSubmit = {onSubmit}>
-<input type ="text" name ="username" value = {username}
- onChange = {changeHandler}/> <br/>
- <input type ="email" name ="email" value = {email}
- onChange = {changeHandler}/> <br/>
- <input type = "submit" name = "submit"/>
+import React, { useState } from 'react';
 
- </form>
+const App = () => {
+    const [data, setData] = useState({
+        username: "",
+        email: ""
+    });
+
+    const [submittedData, setSubmittedData] = useState(null);
+
+    const { username, email } = data;
+
+    const changeHandler = (e) => {
+        setData({ ...data, [e.target.name]: e.target.value });
+    };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        if (username.length <= 5) {
+            alert("Username must be at least 6 characters.");
+        } else {
+            alert("Form Submitted Successfully");
+            setSubmittedData(data); // Store submitted data
+        }
+    };
+
+    return (
+        <div>
+            <center>
+                <form onSubmit={onSubmit}>
+                    <input
+                        type="text"
+                        name="username"
+                        value={username}
+                        placeholder="Enter username"
+                        onChange={changeHandler}
+                    />{" "}
+                    <br />
+                    <input
+                        type="email"
+                        name="email"
+                        value={email}
+                        placeholder="Enter email"
+                        onChange={changeHandler}
+                    />{" "}
+                    <br />
+                    <button type="submit">Submit</button>
+                </form>
+
+                <h3>Submitted Data</h3>
+                {/* Render submitted data conditionally */}
+                {submittedData && (
+                    <div>
+                        <p>
+                            <strong>Name:</strong> {submittedData.username}
+                        </p>
+                        <p>
+                            <strong>Email:</strong> {submittedData.email}
+                        </p>
+                    </div>
+                )}
             </center>
         </div>
-    )
-
+    );
 };
-export default App
+
+export default App;
